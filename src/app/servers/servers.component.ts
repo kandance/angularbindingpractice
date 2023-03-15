0576b8db365fe8692c62b3529bc2360eb7008841
Kandance Ferguson <kandance.ferguson@gmail.com>
2023-03-15 11:40:50 -0400

    one way data binding testing

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdatedServerName(event: Event) {
+    //explictly indicated that it's an HTML element to angular
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
